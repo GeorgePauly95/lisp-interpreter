b874186ac802f6e36e9ea068f74e3b3103940b66
george <georgepauly1995@gmail.com>
2025-08-20 17:12:03 +0530

    added variable definition

--- parser.py
+++ parser.py
@@ -1,3 +1,6 @@
+from operations import add, sub, mul, truerdiv, less_than, great_than
+
+
 def tokenize(command):
     tokens = command.replace("(", " ( ").replace(")", " ) ").split(" ")
     return [token for token in tokens if token != " " and token != ""]
@@ -9,14 +12,16 @@ def read_from_tokens(tokens):
     AST = []
     if len(tokens) == 1:
         return atomize(tokens[0])
-    elif tokens[0] == "(":
-        return read_from_tokens(tokens[1:])
+    # elif tokens[0] == "(":
+    #     return read_from_tokens(tokens[1:])
     token = tokens.pop(0)
     while token != ")":
         if token == "(":
+            print(f"output is: {AST}")
             AST.append(read_from_tokens(tokens))
         else:
             AST.append(atomize(token))
+        print(f"tokens are: {tokens}")
         token = tokens.pop(0)
     return AST
 
@@ -28,79 +33,7 @@ def atomize(token):
         try:
             return float(token)
         except ValueError:
-            return symbolize(token)
-
-
-def symbolize(token):
-    if token in operations:
-        return token
-    return "Invalid Token"
-
-
-def add(*operands):
-    try:
-        result = operands[0]
-        for operand in operands[1:]:
-            result += operand
-        return result
-    except TypeError:
-        return "Invalid Input"
-
-
-def sub(*operands):
-    try:
-        if len(operands) == 1:
-            return 0 - operands[0]
-        result = operands[0]
-        for operand in operands[1:]:
-            result -= operand
-        return result
-    except TypeError:
-        return "Invalid Input"
-
-
-def mul(*operands):
-    try:
-        result = operands[0]
-        for operand in operands[1:]:
-            result *= operand
-        return result
-    except TypeError:
-        return "Invalid Input"
-
-
-def truerdiv(*operands):
-    try:
-        if len(operands) == 1:
-            return 1 / operands[0]
-        result = operands[0]
-        for operand in operands[1:]:
-            result /= operand
-        return result
-    except TypeError:
-        return "Invalid Input"
-
-
-def great_than(*operands):
-    if len(operands) <= 1:
-        return True
-    first_oprd, rem_oprds = operands[0], operands[1:]
-    result = True
-    for oprd in rem_oprds:
-        if first_oprd < oprd:
-            return False
-    return result
-
-
-def less_than(*operands):
-    if len(operands) <= 1:
-        return True
-    first_oprd, rem_oprds = operands[0], operands[1:]
-    result = True
-    for oprd in rem_oprds:
-        if first_oprd > oprd:
-            return False
-    return result
+            return token
 
 
 operations = {
@@ -111,13 +44,18 @@ operations = {
     ">": great_than,
     "<": less_than,
     "if": "if",
+    "define": "define",
+    "lambda": "lambda",
 }
 
 
-def evaluate(AST):
+def evaluate(AST, vars={}):
+    print(f"{AST}")
     if type(AST) is not list:
         if type(AST) is int or type(AST) is float:
             return AST
+        if AST in vars:
+            return vars[AST]
         if AST in operations:
             return operations[AST]
     if len(AST) == 1:
@@ -131,11 +69,11 @@ def evaluate(AST):
             return True
         elif AST[0] == "/":
             return "Invalid Input"
-    if AST[0] not in operations:
-        return "Invalid Input"
-    if "Invalid Token" in AST:
-        return "Invalid Input"
-    # Special forms
+        operands = AST[1:]
+        if len(operands) != 3:
+            return "Invalid Input"
+    # special forms
+    # if
     if AST[0] == "if":
         operands = AST[1:]
         if len(operands) != 3:
@@ -144,11 +82,36 @@ def evaluate(AST):
         if evaluate(predicate) is True:
             return evaluate(consequent)
         return evaluate(alternate)
+    # define
+    if AST[0] == "define":
+        if len(AST) != 3:
+            return "Invalid Input"
+        if AST[1] is not list:
+            vars[AST[1]] = AST[2]
+            return
+    if AST[0] not in operations:
+        return "Invalid Input"
+    # lambda
+    if AST[0][0] == "lambda":
+        arguments = AST[0][1]
+        arguments_values = AST[1:]
+        if len(arguments) != len(arguments_values):
+            return "Invalid Input"
+        n = len(arguments)
+        for i in range(n):
+            vars[arguments[i]] = arguments_values[i]
+        vars["lambda"] = AST[0][-1]
+        return evaluate(vars["lambda"])
+
     # General
     operation, operands = operations[AST[0]], AST[1:]
     return operation(
         *[
-            evaluate(operand) if type(operand) is list else operand
+            evaluate(operand)
+            if type(operand) is list
+            else vars[operand]
+            if operand in vars
+            else operand
             for operand in operands
         ]
     )
