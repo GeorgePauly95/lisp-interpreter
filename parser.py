51acf8060aab44d673cd8fd83ea388625294de8c
george <georgepauly1995@gmail.com>
2025-08-30 19:20:50 +0530

    Deleted old parser

--- parser.py
+++ parser.py
@@ -36,7 +36,7 @@ def atomize(token):
             return token
 
 
-operations = {
+operaters = {
     "+": add,
     "-": sub,
     "*": mul,
@@ -45,81 +45,114 @@ operations = {
     "<": less_than,
 }
 
-special_forms = {"if": "if", "define": "define", "lambda": "lambda"}
+
+def define_special_form(operater, operands, global_env):
+    if len(operands) != 2:
+        return "define Invalid Input"
+    name = operands[0]
+    if type(name) is not list:
+        body = evaluate(operands[1], global_env)
+        if type(body) is not tuple:
+            global_env[name] = body
+            return f"{name} is saved as {body}"
+        local_env = body[0]
+        global_env[name] = local_env, body[1]
+        return f"{name} is saved as {body[1]}"
+    name = operands[0][0]
+    variables = operands[0][1:]
+    local_env = {variable: None for variable in variables}
+    body = operands[1]
+    global_env[name] = local_env, body
+    return f"{name} is saved as {body}"
 
 
-def evaluate(AST, vars={}):
-    if base_cases_checker(AST, vars) is not None:
-        return base_cases_checker(AST, vars)
-    operation, operands = AST[0], AST[1:]
-
-    if operation == "if":
-        # operands = AST[1:]
-        if len(operands) == 2:
-            predicate, consequent = operands[0], operands[1]
-            if evaluate(predicate) is True:
-                return evaluate(consequent)
-            return
-        elif len(operands) == 3:
-            predicate, consequent, alternate = operands[0], operands[1], operands[2]
-            if evaluate(predicate) is True:
-                return evaluate(consequent)
-            return evaluate(alternate)
-        return "Ill formed special form"
-
-    elif operation == "define":
-        if len(AST) != 3:
-            return "Invalid Input"
-        name = AST[1]
-        body = AST[2]
-        if type(name) is not list:
-            vars[name] = evaluate(body)
-            return
-        name = AST[1][0]
-        arguments = AST[1][1:]
-        vars[name] = body
-        print(f"{vars}")
+def if_special_form(operater, operands, global_env):
+    if len(operands) == 2:
+        predicate, consequent = operands[0], operands[1]
+        if evaluate(predicate, global_env) is True:
+            return evaluate(consequent)
         return
+    elif len(operands) == 3:
+        predicate, consequent, alternate = operands[0], operands[1], operands[2]
+        if evaluate(predicate, global_env) is True:
+            return evaluate(consequent, global_env)
+        return evaluate(alternate, global_env)
+    return "Ill formed special form"
 
-    if type(operation) is list:
-        if operation[0] == "lambda":
-            arguments = operation[1]
-            arguments_values = AST[1:]
-            if len(arguments) != len(arguments_values):
-                return "Wrong number of arguments passed to procedure"
-            n = len(arguments)
-            for i in range(n):
-                vars[arguments[i]] = arguments_values[i]
-            vars["lambda"] = operation[-1]
-            return evaluate(vars["lambda"])
 
-    if type(operation) is not list:
-        if operation in operations:
-            return operations[operation](
-                *[
-                    evaluate(operand)
-                    if type(operand) is list
-                    else vars[operand]
-                    if operand in vars
-                    else operand
-                    for operand in operands
-                ]
+def lambda_special_form(operater, operands, global_env, local_env={}):
+    if type(operater) is not list:
+        variables = operands[0]
+        if type(variables) is not list:
+            return "lambda Invalid Input 1"
+        local_env = {variable: None for variable in variables}
+        return local_env, operands[-1]
+    variables = operater[1]
+    if type(variables) is not list:
+        return "lambda Invalid Input 2"
+    values = operands
+    if len(variables) != len(values):
+        return "Wrong number of arguments passed to procedure"
+    n = len(variables)
+    for i in range(n):
+        global_env[variables[i]] = values[i]
+    global_env["lambda"] = operater[-1]
+    return evaluate(global_env["lambda"])
+
+
+special_forms = {
+    "if": if_special_form,
+    "define": define_special_form,
+    "lambda": lambda_special_form,
+}
+
+
+def evaluate(AST, global_env={}):
+    if base_cases_checker(AST, global_env) is not None:
+        return base_cases_checker(AST, global_env)
+    operater, operands = AST[0], AST[1:]
+    if type(operater) is not list:
+        if operater in operaters:
+            return operaters[operater](*evaluate_operands(operands, global_env))
+        elif operater in special_forms:
+            return special_forms[operater](operater, operands, global_env)
+        elif operater in global_env:
+            # print(
+            #     f"The operater is: {operater}\n The global environment is: {global_env}"
+            # )
+            if type(global_env[operater]) is not tuple:
+                return evaluate(operater, global_env)(*operands)
+            local_env = global_env[operater][0]
+            if len(local_env) != len(operands):
+                return "Incorrect number of arguments"
+            local_env = {
+                variable: evaluate(value, global_env)
+                for variable, value in zip(list(local_env.keys()), operands)
+            }
+            # print(f"The local env: {local_env}")
+            operater = global_env[operater][1]
+            local_env.update(
+                {a: global_env[a] for a in global_env if a not in local_env}
             )
-        elif operation in vars:
-            print(f"{operation}")
-            print(f"{vars[operation]}")
-            print(f"{evaluate(vars[operation])}")
+            return evaluate(operater, global_env=local_env)
+    print(operater[0])
+    # ((lambda (x y) (*  x y) 1)
+    if operater[0] in special_forms:
+        return special_forms[operater[0]](operater, operands, global_env, local_env={})
+    new_operater = evaluate(operater)
+    return new_operater(*evaluate_operands(operands, global_env))
 
-    return evaluate(operation)(
-        *[
-            evaluate(operand)
-            if type(operand) is list
-            else vars[operand]
-            if operand in vars
-            else operand
-            for operand in operands
-        ]
-    )
+
+def evaluate_operands(operands, global_env):
+    result = [
+        evaluate(operand, global_env)
+        if type(operand) is list
+        else global_env[operand]
+        if operand in global_env
+        else operand
+        for operand in operands
+    ]
+    return result
 
 
 def lisp_interpreter(input):
--- test.py
+++ test.py
@@ -2,39 +2,43 @@ from parser_2 import lisp_interpreter
 
 tests = [
     # lambda test cases:
-    "(lambda (x) ((lambda (y) (* y 2)) x) 23)",
-    # 'begin' test cases:
-    "(begin (define sum +) (sum 1 2))",
-    # 'define' test cases:
-    # 1
-    "(define sum +)",
-    "sum",
-    "(sum 1 2)",
-    # 2
-    "(define x 7)",
-    "(* x 2)",
-    # 3
-    "(define x (* 1 (+ 1 1)))",
-    "(* x x)",
-    # 4
-    "(define (square x) (* x x))",
-    "(square (* 3 3))",
-    # 5
-    "(define square (lambda (y) (* y y)))",
-    "(square 2)",
-    # 'if' test cases:
-    # 1
-    "(if (> 4 3) 9)",
-    # scoping
-    # 1
+    # "((lambda (x) (* x x)) 3)",
+    "(lambda (x) (lambda (y) (* y x)))",
+    "((lambda (x) (lambda (y) (* y x))) 23)",
+    "(((lambda (x) (lambda (y) (* y x))) 23) 3)",
+    # "(((lambda (x) (lambda (y) (* y x)) 23) 3)",
+    # # 'begin' test cases:
+    # "(begin (define sum +) (sum 1 2))",
+    # # 'define' test cases:
+    # # 1
+    # "(define sum +)",
+    # "sum",
+    # "(sum 1 2)",
+    # # 2
+    # "(define x 7)",
+    # "(* x 2)",
+    # # 3
+    # "(define x (* 1 (+ 1 1)))",
+    # "(* x x)",
+    # # 4
+    # "(define (square x) (* x x))",
+    # "(square (* 3 3))",
+    # # 5
+    # "(define square (lambda (y) (* y y)))",
+    # "(square 2)",
+    # # 'if' test cases:
+    # # 1
+    # "(if (> 4 3) 9)",
+    # # scoping
+    # # 1
     "(define outer-var 100)",
     "(define (test-scope inner-var) (+ outer-var inner-var))",
     "(test-scope 23)",
-    # 2
-    "(define shadowed 10)",
-    "(define (shadow-test shadowed) shadowed)",
-    "(shadow-test 99)",
-    "shadowed",
+    # # 2
+    # "(define shadowed 10)",
+    # "(define (shadow-test shadowed) shadowed)",
+    # "(shadow-test 99)",
+    # "shadowed",
 ]
 for test in tests:
     print(f"Input: {test}")
@@ -42,3 +46,4 @@ for test in tests:
         print(f"Output: {lisp_interpreter(test)}\n")
     except Exception as e:
         print(f"Error: {e}\n")
+        raise e
